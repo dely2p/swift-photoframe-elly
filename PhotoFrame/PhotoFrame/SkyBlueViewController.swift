b68897ab2982e323acf87a0a63a3d9e98a047ebd
dely2p <lhyal2p@gmail.com>
2018-02-06 15:16:34 +0900

    add IBAction in SkyBlueViewController to send nextView #11
    Segue가 아닌 코드로 버튼을 누르면 화면 전환되도록 만들었다.

---
+++ PhotoFrame/PhotoFrame/SkyBlueViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SkyBlueViewController.swift
+//  PhotoFrame
+//
+//  Created by Eunjin Kim on 2018. 2. 6..
+//  Copyright © 2018년 Eunjin Kim. All rights reserved.
+//
+
+import UIKit
+
+class SkyBlueViewController: UIViewController {
+
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        if let skyBlueView = self.storyboard?.instantiateViewController(withIdentifier: "YellowViewController") {
+            self.present(skyBlueView, animated: true, completion: nil)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
